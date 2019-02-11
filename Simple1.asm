6a6ff90a7ed4d56fab1e0b9f83c7461f42359ef4
jc5716 <jc5716@skbl-422-016.ph.ic.ac.uk>
2019-02-11 16:20:48 +0000

    counter works oscilloscope

--- Simple1.asm
+++ Simple1.asm
@@ -6,17 +6,23 @@ rst	code	0x0000	; reset vector
 int_hi	code	0x0008	; high vector, no low vector
 	btfss	INTCON,TMR0IF	; check that this is timer0 interrupt
 	retfie	FAST		; if not then return
+	movlw	0x00
+	movwf	LATB, ACCESS
 	incf	LATD		; increment PORTD
+	movlw	0x01
+	movwf	LATB, ACCESS
 	bcf	INTCON,TMR0IF	; clear interrupt flag
 	retfie	FAST		; fast return from interrupt
 
 main	code
 start	clrf	TRISD		; Set PORTD as all outputs
 	clrf	LATD		; Clear PORTD outputs
+	clrf	TRISB		; Set PORTB as all outputs
+	clrf	LATB		
 	movlw	b'10000111'	; Set timer0 to 16-bit, Fosc/4/256
 	movwf	T0CON		; = 62.5KHz clock rate, approx 1sec rollover
 	bsf	INTCON,TMR0IE	; Enable timer0 interrupt
 	bsf	INTCON,GIE	; Enable all interrupts
+	
 	goto	$		; Sit in infinite loop
-
 	end
